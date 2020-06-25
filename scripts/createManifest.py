f4feb41258b647c7094189bab869d6b9025f8181
Stephen Dade <stephen_dade@hotmail.com>
2020-06-26 09:42:18 +1000

    CI: Generate and upload manifests to CI

--- scripts/createManifest.py
+++ scripts/createManifest.py
@@ -14,19 +14,22 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Manifest generator for ArduPilot user alerts')
 
     # Add the arguments
-    parser.add_argument('-output', action="store", dest="output", default="exampleManifest.json", help="Output manifest file")
-    parser.add_argument('-input', action="store", dest="input", default="examples", help="Input folder of alerts")
+    parser.add_argument('-outputFile', action="store", dest="output", default="exampleManifest.json", help="Output manifest file")
+    parser.add_argument('-inputFolder', action="store", dest="input", default="examples", help="Input folder of alerts")
     
     args = parser.parse_args()
 
-    fullFolder = os.path.join(pathlib.Path(__file__).parent.absolute(), "..", args.input)
-    manifest = os.path.join(pathlib.Path(__file__).parent.absolute(), "..", args.output)
+    inFolder = os.path.join(pathlib.Path(__file__).parent.absolute(), "..", args.input)
+    OutFile = os.path.join(pathlib.Path(__file__).parent.absolute(), "..", "output", args.output)
     manifestData = {}
     numAlerts = 0
 
-    files = [f for f in os.listdir(fullFolder) if os.path.isfile(os.path.join(fullFolder, f))]
+    # create output dir if required
+    pathlib.Path(os.path.dirname(OutFile)).mkdir(parents=True, exist_ok=True)
+
+    files = [f for f in os.listdir(inFolder) if os.path.isfile(os.path.join(inFolder, f))]
     for file in files:
-        fileAbs = os.path.join(fullFolder, file)
+        fileAbs = os.path.join(inFolder, file)
         print("Checking {0}".format(file))
         data = None
 
@@ -40,7 +43,7 @@ if __name__ == "__main__":
                 except json.decoder.JSONDecodeError:
                     print("Bad json")
     # and save
-    with open(manifest, 'w') as outfile:
-        json.dump(manifestData, outfile)
-    print("Saved {0} user alerts to {1}".format(numAlerts, manifest))
+    with open(OutFile, 'w') as outfileHandle:
+        json.dump(manifestData, outfileHandle)
+    print("Saved {0} user alerts to {1}".format(numAlerts, OutFile))
 
